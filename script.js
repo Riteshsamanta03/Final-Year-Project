e4646fccab49d58f2db950121d32142de11bb188
Ritesh Samanta <138030869+Riteshsamanta03@users.noreply.github.com>
2025-09-20 11:44:52 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll(".service-card").forEach((card) => {
-  card.addEventListener("mouseover", () => {
-    card.style.background = "#e0f0ff";
-  });
-  card.addEventListener("mouseout", () => {
-    card.style.background = "#f4f9ff";
-  });
-});
